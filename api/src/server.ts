70df8f627db91197e30bf03f9f3babda2e172493
Ricardo Alves <ricardospalves@gmail.com>
2023-09-20 12:24:47 -0300

    chore(api): add fastify cors package

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,7 @@
 import './dotenvConfig'
 import { fastify } from 'fastify'
 import { fastifyMultipart } from '@fastify/multipart'
+import { fastifyCors } from '@fastify/cors'
 import { getAllPromptsRoute } from './routes/get-all-prompts'
 import { uploadVideoRoute } from './routes/upload-video'
 import { createTranscriptionRoute } from './routes/create-transcription'
@@ -9,6 +10,10 @@ import { generateAICompletionRoute } from './routes/generate-ai-completion'
 const PORT = 3333
 const MEGABYTE = 1_048_576
 const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.register(fastifyMultipart, {
   limits: {
